d78f9f11831e4d423d9fdbaafb79fd2989f3c744
nairitprachanda <nairitprachanda@gmail.com>
2020-10-12 08:45:17 +0530

    [nairit] refactor: checking first name

---
+++ UserRegistration/UserRegistration/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace UserRegistration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the User Registration Problem!");
+            ValidateUserRegistration validateUserRegistration = new ValidateUserRegistration();
+            Console.WriteLine("Enter your first name: ");
+            string firstName = Console.ReadLine();
+            bool firstNameResult = validateUserRegistration.ValidateFirstName(firstName);
+            validateUserRegistration.PrintResult(firstNameResult);
+        }
+    }
+}
---
+++ UserRegistration/UserRegistration/ValidateUserRegistration.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UserRegistration
+{
+    class ValidateUserRegistration
+    {
+        private static readonly string Regex_FirstName = "^[A-Z]{1}[a-z]{2,}$";
+        public bool ValidateFirstName(string FN)
+        {
+            return Regex.IsMatch(FN, Regex_FirstName);
+        }
+        public void PrintResult(bool result)
+        {
+            if (result)
+            {
+                Console.WriteLine("Valid name");
+            }
+            else
+            {
+                Console.WriteLine("Invalid name");
+            }
+        }
+    }
+}
+
+
