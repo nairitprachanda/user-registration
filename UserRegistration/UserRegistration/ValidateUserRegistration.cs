c784835109ac1c9cb172e60a6f6024503088014c
nairitprachanda <nairitprachanda@gmail.com>
2020-10-13 08:37:30 +0530

    [nairit] refactor: checking last name

--- UserRegistration/UserRegistration/ValidateUserRegistration.cs
+++ UserRegistration/UserRegistration/ValidateUserRegistration.cs
@@ -1,28 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
-
-namespace UserRegistration
+using System.Text.RegularExpressions;
+namespace User_Registration_Problem
 {
     class ValidateUserRegistration
     {
         private static readonly string Regex_FirstName = "^[A-Z]{1}[a-z]{2,}$";
+        private static readonly string Regex_LastName = "^[A-Z]{1}[a-z]{2,}$";
         public bool ValidateFirstName(string FN)
         {
             return Regex.IsMatch(FN, Regex_FirstName);
+        }
+        public bool ValidateLastName(string LN)
+        {
+            return Regex.IsMatch(LN, Regex_LastName);
         }
         public void PrintResult(bool result)
         {
             if (result)
             {
-                Console.WriteLine("Valid name");
+                Console.WriteLine("Valid name.");
             }
             else
             {
-                Console.WriteLine("Invalid name");
+                Console.WriteLine("Invalid name.");
             }
         }
     }
 }
-
-
