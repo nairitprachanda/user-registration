c38a42bdb5fac23ebb68f50d7b182c0335d14052
nairitprachanda <nairitprachanda@gmail.com>
2020-10-13 08:44:27 +0530

    [nairit] refactor: entering email id

--- UserRegistration/UserRegistration/Program.cs
+++ UserRegistration/UserRegistration/Program.cs
@@ -1,17 +1,26 @@
 ﻿using System;
 
-namespace UserRegistration
+namespace User_Registration_Problem
 {
     class Program
     {
         static void Main(string[] args)
         {
+            //Welcome msg
             Console.WriteLine("Welcome to the User Registration Problem!");
             ValidateUserRegistration validateUserRegistration = new ValidateUserRegistration();
-            Console.WriteLine("Enter your first name: ");
+            Console.WriteLine("Enter your first name : ");
             string firstName = Console.ReadLine();
             bool firstNameResult = validateUserRegistration.ValidateFirstName(firstName);
             validateUserRegistration.PrintResult(firstNameResult);
+            Console.WriteLine("Enter your last name : ");
+            string lastName = Console.ReadLine();
+            bool lastNameResult = validateUserRegistration.ValidateLastName(lastName);
+            validateUserRegistration.PrintResult(lastNameResult);
+            Console.WriteLine("Enter your email : ");
+            string eMail = Console.ReadLine();
+            bool eMailResult = validateUserRegistration.ValidateEmail(eMail);
+            validateUserRegistration.PrintResult(eMailResult);
         }
     }
 }
--- UserRegistration/UserRegistration/ValidateUserRegistration.cs
+++ UserRegistration/UserRegistration/ValidateUserRegistration.cs
@@ -8,6 +8,7 @@ namespace User_Registration_Problem
     {
         private static readonly string Regex_FirstName = "^[A-Z]{1}[a-z]{2,}$";
         private static readonly string Regex_LastName = "^[A-Z]{1}[a-z]{2,}$";
+        private static readonly string Regex_Email = "^[0-9a-zA-Z]{1,}([._+-][0-9a-zA-Z]{0,})*[@][0-9a-zA-Z]{1,}.[a-zA-Z]{2,3}(.[a-zA-Z]{2,3})?$";
         public bool ValidateFirstName(string FN)
         {
             return Regex.IsMatch(FN, Regex_FirstName);
@@ -16,16 +17,20 @@ namespace User_Registration_Problem
         {
             return Regex.IsMatch(LN, Regex_LastName);
         }
+        public bool ValidateEmail(string Email)
+        {
+            return Regex.IsMatch(Email, Regex_Email);
+        }
         public void PrintResult(bool result)
         {
             if (result)
             {
-                Console.WriteLine("Valid name.");
+                Console.WriteLine("Valid input");
             }
             else
             {
-                Console.WriteLine("Invalid name.");
+                Console.WriteLine("Invalid input");
             }
         }
     }
-}
+}    
